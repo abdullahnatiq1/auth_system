2f8099b1e8ecf64e533cca2972d2825686482cbc
Abdullah Natiq <abdullahnatiq8765@gmail.com>
2026-02-24 12:20:50 +0500

    uuid id implemetation and generated auto id

--- routes.py
+++ routes.py
@@ -1,17 +1,34 @@
 from fastapi import APIRouter
 from db import getDbConnection   #  yeh humne password ki encryption krna k liya library install ki hai
 from psycopg2.extras import RealDictCursor
-import bcrypt
+import bcrypt                    # bcrypt is only used for one way hased it can't be reversed
+                                 # to decrypt and encrypt both use import fernet it's two way encryption
+from jose import jwt             # used to create and verify tokens
+from datetime import datetime, timedelta     # datetime  -> to get current time, timedelta -> to add time(like 24 hours form now)
+import uuid
+
+
+secretKey = "your-secret-key"
+algorithm = "HS256"              # HS256 is an algorithm used to generate random tokens
+
+def createToken(data : dict):    # yahan hum aik function bana raha hai or data k aik variable bhhi create kiya hai 
+    # or hume data ko dictionary is liya rakha hai q k jwt.encode only takes dict as input
+    expire = datetime.utcnow() + timedelta(hours = 24)    # utcnow humain current date and time deta hai
+    # and timedelta (hours = 24) ye utctime main 24 hours add krta hai so it expires after 24
+    data.update({"exp" : expire})  # data main already id and email hai so .update usme exp bhi add kr raha hai
+    token = jwt.encode(data, secretKey, algorithm = algorithm)
+    return token
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
-
 @router.post("/signup")
-def signup(id : int, username : str, email : str, password : str):
+def signup(username : str, email : str, password : str):
     conn = getDbConnection()
     cursor = conn.cursor()
 
     try:
+        userid = str(uuid.uuid4())
+        
         hashedPassword = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
         checkQuery = "select * from users where email = %s"
@@ -21,8 +38,8 @@ def signup(id : int, username : str, email : str, password : str):
         if existingUser:
             return{"message" : "User already exists with this email. Please Login"}
 
-        insertQuery = "insert into users (id, username, email, password) values (%s, %s, %s, %s)" 
-        cursor.execute(insertQuery,(id, username, email, hashedPassword))
+        insertQuery = "insert into users (uuid, username, email, password) values (%s, %s, %s, %s)" 
+        cursor.execute(insertQuery,(userid, username, email, hashedPassword))
         conn.commit()
         return {"message" : "User created successfully" }
 
@@ -41,7 +58,7 @@ def signin(email: str, password: str):
 
     try:
         # 1. Fetch user by email
-        query = "SELECT password FROM users WHERE email = %s"
+        query = query = "SELECT id, username, email, password FROM users WHERE email = %s"
         cursor.execute(query, (email,))
         user = cursor.fetchone()
 
@@ -57,7 +74,18 @@ def signin(email: str, password: str):
         user_password_bytes = password.encode('utf-8')
 
         if bcrypt.checkpw(user_password_bytes, stored_hash):
-            return {"login": user}
+            token = createToken({"id" : user["id"], "email" : user["email"]})
+            return {
+                "message" : "Login successful",
+                "token" : token,
+                "user" : {
+                    "id" : user["id"],
+                    "username" : user["username"],
+                    "email" : user["email"]
+                }
+
+
+            }
         else:
             return {"message": "Invalid credentials"}
 
